953bb641808a7ba78b9076608d6f82e4152fb7b2
partha99saha <psaha.bdn9@gmail.com>
2022-04-06 12:36:12 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,8 +80,8 @@ let education = document.getElementById("education");
 let work = document.getElementById("work");
 let educationheader = document.getElementById("educationheader");
 let workheader = document.getElementById("workheader");
-workheader.style.color = "var(--first-color)";
-educationheader.style.color = "var(--text-color)";
+workheader.style.color = "var(--text-color)";
+educationheader.style.color = "var(--first-color)";
 
 educationheader.addEventListener("click", () => {
   let condition1 = work.classList.contains("qualification-inactive");
@@ -195,7 +195,7 @@ themeButton.addEventListener("click", () => {
 
 // Typing Animation using Typed JS
 var typed = new Typed(".type", {
-  strings: [" Software Developer", " tech Enthusiast", " a programmer"],
+  strings: [" Software Developer", " Tech Enthusiast", " a Programmer"],
   smartBackspace: true,
   startDelay: 1000,
   typeSpeed: 130,
